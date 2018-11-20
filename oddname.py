4c6d3708506ebf31e440c99e65b970b96ffd4b02
jc506937 <shailey.chawla@jcu.edu.au>
2018-11-20 11:31:29 +0800

    Refactor getname and print name

--- oddname.py
+++ oddname.py
@@ -1,12 +1,23 @@
 """Input name and print odd characters"""
+def main():
 
-name=input("Enter name:- ")
+    name = get_name()#print odd characters in the name
+    num=int(input("enter indexes to skip 1-continuous,2-alternate.."))
+    print_name(name, num)
+    #print odd characters in the name
 
-#check that name is not blank
-while len(name)<=0:
-    print("Name is blank, enter again ")
-    name=input("Enter name:- ")
-#print odd characters in the name
-print(name[::2])
-#print odd characters in the name
 
+def print_name(name, num):
+    print(name[::num])
+
+
+def get_name():
+    name = input("Enter name:- ")
+    # check that name is not blank
+    while len(name) <= 0:
+        print("Name is blank, enter again ")
+        name = input("Enter name:- ")
+    return name
+
+
+main()
